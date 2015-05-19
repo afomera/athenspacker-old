37935a30b549637fc66af534dd21aad035762e8b
King601 <andrew@zerlex.net>
2015-05-18 22:57:53 -0500

    Remove mod after zipping, also move zipfile_name to one line

--- AthensPacker.rb
+++ AthensPacker.rb
@@ -19,8 +19,8 @@ def create_mods_folder
 end
 
 def move_to_mods_folder(filename)
-	logger.debug("Moving #{filename}")
 	create_mods_folder
+	logger.debug("Moving #{filename}")
 	FileUtils::cp_r("#{filename}", "mods/")
 	logger.debug("Moved #{filename} to mods/")
 end
@@ -53,7 +53,7 @@ if directory_exists?("mods") then
 end
 
 logger.info("Checking if there are mods to package...")
-print Dir.glob("to_package/*") unless Dir.glob("to_package/*").empty?
+print Dir.glob("to_package/*").sort unless Dir.glob("to_package/*").empty?
 
 if Dir.glob("to_package/*").empty? 
 	logger.fatal ("There are no mods to package... aborting")
@@ -66,15 +66,15 @@ to_package_files.each do |to_package|
 	logger.debug("Found file #{to_package}")
 	move_to_mods_folder(to_package)
 	logger.info("zipping file.. #{to_package}")
-
-	zipfile_name = to_package.chomp(".jar").chomp(".zip").downcase + ".zip"
-	# Get rid of spaces and quotes in filenames.
-	zipfile_name = zipfile_name.tr(" '\"", "")
+	# Downcase the filename, remove .jar and .zip 
+	# Then remove spaces and quotes then add .zip so the zipfile can be created & properly named
+	zipfile_name = to_package.downcase.chomp(".jar").chomp(".zip").tr(" '\"", "") + ".zip"
 
 	system("zip -r '#{zipfile_name}' 'mods/'")
 	logger.debug("Moving #{zipfile_name}")
 	puts zipfile_name
 	FileUtils::mv("#{zipfile_name}", "to_upload/")
+	FileUtils::rm("#{to_package}") # Remove other file
 	logger.info("Now removing file.. #{to_package} in mods")
 	remove_mods_folder
 end
