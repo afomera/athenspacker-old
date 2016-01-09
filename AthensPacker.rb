f88007dcd8f6c9100b6ccd987dcf0e25b5961895
Andrew Fomera <andrew@zerlex.net>
2016-01-08 23:08:25 -0600

    rename the to_package block var as mod because it better reflects that its working on a mod itself

--- AthensPacker.rb
+++ AthensPacker.rb
@@ -31,23 +31,23 @@ def remove_mods_folder
 end
 
 
-if !directory_exists?("to_package") then 
+if !directory_exists?("to_package") then
 	logger.fatal("The to_package folder does not exist")
 	logger.info("Creating to_package folder then...")
 	FileUtils::mkdir_p 'to_package/'
-elsif directory_exists?("to_package") then 
-	logger.info("The to_package folder exists so skipping creating!") 
+elsif directory_exists?("to_package") then
+	logger.info("The to_package folder exists so skipping creating!")
 end
 
-if !directory_exists?("to_upload") then 
+if !directory_exists?("to_upload") then
 	logger.fatal("The to_upload folder does not exist")
 	logger.info("Creating to_upload folder then...")
 	FileUtils::mkdir_p 'to_upload/'
-elsif directory_exists?("to_upload") then 
-	logger.info("The to_upload folder exists so skipping creating!") 
+elsif directory_exists?("to_upload") then
+	logger.info("The to_upload folder exists so skipping creating!")
 end
 
-if directory_exists?("mods") then 
+if directory_exists?("mods") then
 	logger.warn("The mods folder already exist... so deleting")
 	remove_mods_folder
 end
@@ -55,20 +55,20 @@ end
 logger.info("Checking if there are mods to package...")
 print Dir.glob("to_package/*").sort unless Dir.glob("to_package/*").empty?
 
-if Dir.glob("to_package/*").empty? 
+if Dir.glob("to_package/*").empty?
 	logger.fatal ("There are no mods to package... aborting")
 	abort
 end
 
 to_package_files = Dir.glob("to_package/*")
 @to_package_files_count = to_package_files.count.to_s
-to_package_files.each do |to_package|
-	logger.debug("Found file #{to_package}")
-	move_to_mods_folder(to_package)
-	logger.info("zipping file.. #{to_package}")
-	# Downcase the filename, remove .jar and .zip 
+to_package_files.each do |mod|
+	logger.debug("Found file #{mod}")
+	move_to_mods_folder(mod)
+	logger.info("zipping file.. #{mod}")
+	# Downcase the filename, remove .jar and .zip
 	# Then remove spaces and quotes then add .zip so the zipfile can be created & properly named
-	zipfile_name = to_package.downcase.chomp(".jar").chomp(".zip").tr(" '\"", "") + ".zip"
+	zipfile_name = mod.downcase.chomp(".jar").chomp(".zip").tr(" '\"", "") + ".zip"
 
 	system("zip -r '#{zipfile_name}' 'mods/'")
 	logger.debug("Moving #{zipfile_name}")
